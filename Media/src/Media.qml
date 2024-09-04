ed8899476d19d47bd66823729a1d491df9205838
VMNg <nguyenminh121201@gmail.com>
2024-09-04 10:34:03 -0700

    Update baseline core app

---
+++ Media/src/Media.qml
@@ -0,0 +1,21 @@
+import QtQuick 2.15
+import QtQuick.Controls 2.15
+
+Item {
+    width: 1080
+    height: 720
+
+    Rectangle {
+        anchors.fill: parent
+        color: "#1e1e1e"
+
+        Text {
+            anchors.centerIn: parent
+            text: "Media Player"
+            color: "white"
+            font.pixelSize: 24
+        }
+
+        // TODO: Add media player components (album art, track info, playback controls, etc.)
+    }
+}
--- main.cpp
+++ main.cpp
@@ -6,16 +6,12 @@ int main(int argc, char *argv[])
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
-    const QUrl url(QStringLiteral("qrc:/PBL/Main.qml"));
-    QObject::connect(
-        &engine,
-        &QQmlApplicationEngine::objectCreated,
-        &app,
-        [url](QObject *obj, const QUrl &objUrl) {
-            if (!obj && url == objUrl)
-                QCoreApplication::exit(-1);
-        },
-        Qt::QueuedConnection);
+    const QUrl url(u"qrc:/PBL/Main.qml"_qs);
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+                     &app, [url](QObject *obj, const QUrl &objUrl) {
+        if (!obj && url == objUrl)
+            QCoreApplication::exit(-1);
+    }, Qt::QueuedConnection);
     engine.load(url);
 
     return app.exec();
