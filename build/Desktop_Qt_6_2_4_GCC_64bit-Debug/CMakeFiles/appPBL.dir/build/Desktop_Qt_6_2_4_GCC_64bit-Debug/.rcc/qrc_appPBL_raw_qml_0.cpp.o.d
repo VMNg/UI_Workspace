f48d4b76b9a83a2bb08c1c8bd12524c406df4d38
Minh Phi <himinhpho@gmail.com>
2024-09-09 08:43:39 -0700

    Setup build, run shell script, add gitignore

--- build/Desktop_Qt_6_2_4_GCC_64bit-Debug/CMakeFiles/appPBL.dir/build/Desktop_Qt_6_2_4_GCC_64bit-Debug/.rcc/qrc_appPBL_raw_qml_0.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/appPBL.dir/build/Desktop_Qt_6_2_4_GCC_64bit-Debug/.rcc/qrc_appPBL_raw_qml_0.cpp.o: \
- /home/fr/Moc_project/UI_Workspace/UI_Workspace/build/Desktop_Qt_6_2_4_GCC_64bit-Debug/.rcc/qrc_appPBL_raw_qml_0.cpp \
- /usr/include/stdc-predef.h
